0a370df354fa1b40149f5c49a62820d232e59e90
yakob21 <yakobgetachew19@gmail.com>
2024-01-04 08:38:18 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
-
 def magic_calculation(a, b):
     add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
 
     if a < b:
         c = add(a, b)
-        for i in range(4, 6):
+        for i in range(4, 7):
             c = add(c, i)
         return c
     else:
